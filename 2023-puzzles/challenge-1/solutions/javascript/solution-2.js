a36425b5f3cc094448ad66bb1320beef79c17367
Christian Heilmann <chris.heilmann@gmail.com>
2023-08-02 16:25:58 +0200

    Adding another JS solution to problem one.

---
+++ 2023-puzzles/challenge-1/solutions/javascript/solution-2.js
@@ -0,0 +1,12 @@
+const puzzle = require('../../puzzle.json');
+const result = require('../../result.json');
+
+let solution = 0;
+puzzle.split(/\D/).forEach(v => { solution += +v} );
+
+const isSolutionCorrect = () => {
+    return JSON.stringify(solution) === JSON.stringify(result);
+};
+
+console.log(solution);
+console.log('Is solution correct?', isSolutionCorrect());
