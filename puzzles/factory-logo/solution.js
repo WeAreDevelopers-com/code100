5afef65c997bbe2949684eadd791b98fe1cdc9a3
Christian Heilmann <chris.heilmann@gmail.com>
2024-03-25 19:08:34 +0100

    factory logo challenge

---
+++ puzzles/factory-logo/solution.js
@@ -0,0 +1,14 @@
+let dimensions = {
+    "halls": [100,100],
+    "chimney": [35,120],
+    "connect": [35,25],
+    "roofs": [100,70] 
+}
+let total = 0;
+total += dimensions.halls[0] * dimensions.halls[1] * 3;
+total += ((dimensions.roofs[0] * dimensions.roofs[1]) / 2) * 3;
+total += dimensions.chimney[0] * dimensions.chimney[1];
+total += (dimensions.connect[0] * dimensions.connect[1]) / 2;
+
+console.log(Math.round(total));
+
