a6240d2284536058ee48e72f7558e42a7dabbc50
Christian Heilmann <chris.heilmann@gmail.com>
2024-04-30 12:08:22 +0200

    adding youtube videos

--- bets/leaderboard-grid.js
+++ bets/leaderboard-grid.js
@@ -6,7 +6,7 @@ const drawboard = (challengers) => {
     let linkedinsvg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="1em" height="1em"><title>#title</title><path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path></svg>';
     challengers.forEach((challenger,k) => {
         bets.push({name: challenger[0], bet:(k+1*100), key:'challenger' + (k+1)});
-        let [name, lastname, company, languages, slogan, country, years, linkedin, github] = challenger;
+        let [name, lastname, company, languages, slogan, country, years, linkedin, github,youtube] = challenger;
         let item = document.createElement('div');
         item.classList.add('item');
         item.style.setProperty('--order', (k));
@@ -22,6 +22,9 @@ const drawboard = (challengers) => {
                     ${linkedin ? '<a href="'+linkedin+'" class="linkedin">'+linkedinsvg+'</a>': '&nbsp;'}
                 </h2>
                 <div class="slogan">${slogan}</div>
+                <div class="youtube">
+                ${youtube ? '<a href="'+youtube+'" class="youtube">Watch '+name+'\'s interview</a>': '&nbsp;'}
+                </div>
                 <div class="meta">
                     <!-- <span class="company">${company}</span> -->
                     <span>Languages: ${languages}</span>
