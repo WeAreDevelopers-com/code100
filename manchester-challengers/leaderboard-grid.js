525d1f4dcb2f320c60d7dbdfb8553505c1b1833f
Christian Heilmann <chris.heilmann@gmail.com>
2024-05-06 17:53:49 +0200

    fixing broken images

--- manchester-challengers/leaderboard-grid.js
+++ manchester-challengers/leaderboard-grid.js
@@ -12,7 +12,8 @@ const drawboard = (challengers) => {
         item.style.setProperty('--order', (k));
         item.setAttribute('id', name.toLowerCase().trim()+lastname.toLowerCase().trim());
         item.style.order =  (k + 1);
-        //MARK: item HTML
+        //MARK: item HTML\
+        
         out =`
         <img src="https://puzzles.code100.dev/manchester-challengers/challengers/${name.toLowerCase()}${lastname.toLowerCase()}.jpg" alt="Photo of ${name} ${lastname}">
         <div class="namecontainer">
